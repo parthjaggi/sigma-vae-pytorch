fe1917057189fdf0ab74201b1dbb08b88528d729
Parth <parthjaggi28@gmail.com>
2021-01-25 14:49:47 -0500

    original run utils;

--- train_vae.py
+++ train_vae.py
@@ -15,16 +15,11 @@ https://github.com/pytorch/examples/blob/master/vae/main.py """
 
 ## Arguments
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--model', type=str, default='mse', metavar='N',
-                    help='which model to use: mse_vae,  gaussian_vae, or sigma_vae or optimal_sigma_vae')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
+parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
+parser.add_argument('--model', type=str, default='mse_vae', metavar='N', help='which model to use: mse_vae,  gaussian_vae, or sigma_vae or optimal_sigma_vae')
 parser.add_argument('--log_dir', type=str, default='test', metavar='N', required=True)
 args = parser.parse_args()
 
@@ -34,8 +29,8 @@ device = torch.device("cuda" if args.cuda else "cpu")
 
 ## Dataset
 transform = transforms.Compose([transforms.Resize((28, 28)), transforms.ToTensor()])
-train_dataset = datasets.SVHN('../../data', split='train', download=True, transform=transform)
-test_dataset = datasets.SVHN('../../data', split='train', transform=transform)
+train_dataset = datasets.SVHN('./data', split='train', download=True, transform=transform)
+test_dataset = datasets.SVHN('./data', split='train', transform=transform)
 kwargs = {'num_workers': 10, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
